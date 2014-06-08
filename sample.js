d851f3b23b5d4ce8b93b7f99017dfe2df7c9e289
Jason Denizac <jason@denizac.org>
2014-06-08 10:49:16 -0400

    Update sample.js
    include better error handling from #1

--- sample.js
+++ sample.js
@@ -1,10 +1,11 @@
 var flite = require('./flite');
 
 flite(function (err, speech) {
+  if (err) { return console.error(err) }
   console.log('Available voices:', speech.voices);
 
   speech.say('hello bear! how are you today?', function (err) {
-    if (err) console.error('I\'m afraid I can\'t do that, Dave', err);
+    if (err) { console.error(err) }
   });
 
 });
